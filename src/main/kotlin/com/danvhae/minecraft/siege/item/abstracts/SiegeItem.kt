621b7856a218239c2ab5f4334335df1207bc46c3
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 12:41:53 +0900

    Feat: 매직스펠 아이템 파싱

--- src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/SiegeItem.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/SiegeItem.kt
@@ -1,5 +1,6 @@
 package com.danvhae.minecraft.siege.item.abstracts
 
+import com.danvhae.minecraft.siege.item.items.MagicSpellItem
 import org.bukkit.inventory.ItemStack
 
 abstract class SiegeItem {
@@ -8,6 +9,7 @@ abstract class SiegeItem {
         fun parseItem(stack: ItemStack?):SiegeItem?{
             stack?:return null
             TicketAbstract.parseTicket(stack)?.let{return it}
+            MagicSpellItem.parseItem(stack)?.let{return it}
             return null
         }
     }

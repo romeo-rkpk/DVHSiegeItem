40077a61dd5ce357f2fa2f2b7010bf346fe628b8
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 11:49:23 +0900

    Feat: 야생티켓을 티켓 파싱 목록에 추가함

--- src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/TicketAbstract.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/TicketAbstract.kt
@@ -2,6 +2,7 @@ package com.danvhae.minecraft.siege.item.abstracts
 
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.item.items.AttackTicket
+import com.danvhae.minecraft.siege.item.items.WildTicket
 import com.danvhae.minecraft.siege.item.items.WorkTicket
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
@@ -11,7 +12,7 @@ abstract class TicketAbstract :SiegeItem(){
     companion object{
         fun parseTicket(stack: ItemStack):TicketAbstract?{
 
-
+            WildTicket.parseItem(stack)?.let{return it}
             for(castle in SiegeCastle.DATA.values){
                 for(ticket in listOf(AttackTicket(castle), WorkTicket(castle))){
                     if(ticket.toItemStack().isSimilar(stack))return ticket

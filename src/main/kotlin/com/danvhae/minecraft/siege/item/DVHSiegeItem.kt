0df6fdce70ee8f09130897de22194fe260232375
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 13:32:03 +0900

    Feat: 부적절한 공성 아이템 사용 차단 리스너 작성

--- src/main/kotlin/com/danvhae/minecraft/siege/item/DVHSiegeItem.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/DVHSiegeItem.kt
@@ -4,6 +4,8 @@ import com.danvhae.minecraft.siege.item.commands.SiegeSpellItemCommand
 import com.danvhae.minecraft.siege.item.commands.TicketItemCommand
 import com.danvhae.minecraft.siege.item.completers.SpellItemCompleter
 import com.danvhae.minecraft.siege.item.completers.TicketCompleter
+import com.danvhae.minecraft.siege.item.listeners.IllegalCraftingListener
+import com.danvhae.minecraft.siege.item.listeners.IllegalTradingListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -18,6 +20,10 @@ class DVHSiegeItem : JavaPlugin(){
     override fun onEnable() {
         instance = this
         Bukkit.getLogger().info("단비해 화이팅")
+
+        val pm = Bukkit.getPluginManager()
+        pm.registerEvents(IllegalCraftingListener(), this)
+        pm.registerEvents(IllegalTradingListener(), this)
 
         getCommand("siege-spell-item").executor = SiegeSpellItemCommand()
         getCommand("siege-spell-item").tabCompleter = SpellItemCompleter()

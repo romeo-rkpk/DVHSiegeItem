14ac5c0f5a68bb017f9a2ce4350eb4cf26dbe5a0
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 09:43:19 +0900

    Feat: 티켓 파싱에서 공성 및 수성 티켓 고려함

--- src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/TicketAbstract.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/abstracts/TicketAbstract.kt
@@ -1,5 +1,8 @@
 package com.danvhae.minecraft.siege.item.abstracts
 
+import com.danvhae.minecraft.siege.core.objects.SiegeCastle
+import com.danvhae.minecraft.siege.item.items.AttackTicket
+import com.danvhae.minecraft.siege.item.items.WorkTicket
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
@@ -7,6 +10,14 @@ abstract class TicketAbstract :SiegeItem(){
 
     companion object{
         fun parseTicket(stack: ItemStack):TicketAbstract?{
+
+
+            for(castle in SiegeCastle.DATA.values){
+                for(ticket in listOf(AttackTicket(castle), WorkTicket(castle))){
+                    if(ticket.toItemStack().isSimilar(stack))return ticket
+                }
+            }
+
             return null
         }
     }

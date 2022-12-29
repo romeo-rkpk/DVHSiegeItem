2cf46d5e20343da132f5e65656d84161567d8e06
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 09:39:05 +0900

    Feat: 공성 티켓

--- src/main/kotlin/com/danvhae/minecraft/siege/item/items/AttackTicket.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/items/AttackTicket.kt
@@ -1,11 +1,20 @@
 package com.danvhae.minecraft.siege.item.items
 
+import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.item.abstracts.StarTicketAbstract
 import org.bukkit.entity.Player
 
 class AttackTicket(destination:SiegeCastle) : StarTicketAbstract(destination) {
     override fun useAndTeleport(player: Player) {
-        TODO("Not yet implemented")
+        if(destination.status == SiegeCastleStatus.INIT){
+            player.sendMessage("그 별은 주인이 없습니다")
+            return
+        }else if(destination.status == SiegeCastleStatus.ELIMINATED){
+            player.sendMessage("이미 멸망한 별입니다")
+            return
+        }
+
+        player.teleport(destination.attackPosition)
     }
 }

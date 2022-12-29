2340cadf8e8aaafccb87af199c00e2e2d65ad3f5
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 09:10:43 +0900

    Feat: WIP

--- src/main/kotlin/com/danvhae/minecraft/siege/item/items/WorkTicket.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/items/WorkTicket.kt
@@ -1,11 +1,16 @@
 package com.danvhae.minecraft.siege.item.items
 
+import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.item.abstracts.StarTicketAbstract
 import org.bukkit.entity.Player
 
 class WorkTicket(destination:SiegeCastle) : StarTicketAbstract(destination) {
     override fun useAndTeleport(player: Player) {
-        TODO("Not yet implemented")
+        if(destination.status !in listOf(SiegeCastleStatus.PEACEFUL, SiegeCastleStatus.UNDER_BATTLE))
+            return
+        player.teleport(destination.workPosition)
+        player.sendMessage("${destination.name} 일꾼 텔레포트 지점으로 이동하였습니다")
+
     }
 }

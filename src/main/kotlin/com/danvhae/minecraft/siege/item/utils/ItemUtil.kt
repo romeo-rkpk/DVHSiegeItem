7af627a8c5597995ac57307e2a941030ccea1d98
RomeoRKPK <mindaye8504@gmail.com>
2022-12-29 12:49:54 +0900

    Feat: 인벤토리가 비었을 때 아이템을 주도록 유틸 작성

--- src/main/kotlin/com/danvhae/minecraft/siege/item/utils/ItemUtil.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/utils/ItemUtil.kt
@@ -17,5 +17,24 @@ class ItemUtil {
 
             return null
         }
+
+        fun giveItemIfAvailable(player:Player, item:ItemStack):Boolean{
+            var founded = false
+            if(player.inventory.firstEmpty() != -1){
+                founded = true
+            }else{
+                for(slot in player.inventory){
+                    slot?:continue
+                    if(slot.isSimilar(item) && slot.amount < slot.maxStackSize){
+                        founded = true
+                        break
+                    }
+                }
+            }
+
+            if(founded)
+                player.inventory.addItem(item)
+            return founded
+        }
     }
 }

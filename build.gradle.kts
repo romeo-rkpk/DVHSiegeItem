c9e50daf75ad0a2a63ab786d7b3e1ffc59c7609c
RomeoRKPK <mindaye8504@gmail.com>
2022-12-30 22:48:52 +0900

    Feat: 그래들 변동 반영

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,3 +50,57 @@ tasks.withType<ProcessResources>{
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+tasks.register("단츄", Jar::class){
+    group = "!danvhae"
+    description = "의존성 모음집에 버전명 없이 파일을 내보냅니다"
+    //dependsOn(listOf("jar", "build"))
+
+    dependsOn("build")//.mustRunAfter("clean")
+
+    from("build/classes/kotlin/main")
+    from("build/resources/main")
+
+    println("this is danchu task")
+    doFirst{
+        destinationDirectory.set(file(env.fetch("DEPEND_DIR")))
+        archiveFileName.set(rootProject.name + ".jar")
+    }
+
+}
+
+tasks.register("보라비", Jar::class){
+    group = "!danvhae"
+    description = "테스트 서버에 파일을 내보냅니다. 버전명 없이 내보냅니다."
+    //dependsOn(listOf("jar", "build"))
+
+    dependsOn("build")
+
+    from("build/classes/kotlin/main")
+    from("build/resources/main")
+
+    println("this is borav task")
+    doFirst{
+
+        destinationDirectory.set(file(env.fetch("PLUGIN_DIR")))
+        archiveFileName.set(rootProject.name + ".jar")
+
+    }
+
+}
+
+tasks.register("해야", Jar::class) {
+    group = "!danvhae"
+    description = "어딘가에 있는 일시 폴더에 버전명을 포함하여 내보냅니다."
+    //dependsOn(listOf("jar", "build"))
+    //dependsOn("clean")
+    dependsOn("build")//.mustRunAfter("clean")
+
+    from("build/classes/kotlin/main")
+    from("build/resources/main")
+
+    println("this is haeya task")
+    doFirst {
+        destinationDirectory.set(file(env.fetch("ARCHIVE_DIR")))
+    }
+}

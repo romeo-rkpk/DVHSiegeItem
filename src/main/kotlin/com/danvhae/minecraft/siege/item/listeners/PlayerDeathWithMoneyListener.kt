c3932c8a8f958ca27ef1411fbe7d22d2d169f9d5
RomeoRKPK <mindaye8504@gmail.com>
2022-12-31 19:24:10 +0900

    Feat: 리스너 작성 완료

--- src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/PlayerDeathWithMoneyListener.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/PlayerDeathWithMoneyListener.kt
@@ -1,13 +1,50 @@
 package com.danvhae.minecraft.siege.item.listeners
 
+import com.danvhae.minecraft.siege.core.DVHSiegeCore
 import com.danvhae.minecraft.siege.core.utils.FileUtil
+import com.danvhae.minecraft.siege.core.utils.TextUtil
+import com.danvhae.minecraft.siege.item.items.Cheque
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
+import java.util.Random
+import kotlin.math.min
+import kotlin.math.tanh
+
 
 class PlayerDeathWithMoneyListener {
 
+    private val config:MoneyConfig = MoneyConfig.load()
+
+    @EventHandler
+    fun onPlayerDeath(event:PlayerDeathEvent){
+        val ratio = (0.5 * (1 + tanh(Random().nextGaussian())) / (Math.PI * 0.5))
+            .let { r -> r * config.minRatio + (1 - r) * config.maxRatio }
+
+        val economy = DVHSiegeCore.economy!!
+        val player = event.entity
+        for(stack in player.inventory){
+            val cheque = Cheque.parseItem(stack?:continue)?:continue
+            economy.depositPlayer(player, cheque.amount * stack.amount.toDouble())
+            stack.amount = 0
+        }
+
+        val dropAmount = min((economy.getBalance(player) * ratio).toInt(), config.maxAmount)
+        if(dropAmount <= 0) return
+
+        economy.withdrawPlayer(player, dropAmount.toDouble())
+
+        val cheque = Cheque(dropAmount, TextUtil.toColor("&f&4%s&f&l가 주머니에서 흘린 &6&l돈이다.".format(player.name)) )
+        player.location.let { loc -> loc.world.dropItem(loc, cheque.toItemStack()) }
+
+    }
+
     /**
      * 플레이어가 죽을 때 스타를 얼마나 떨어뜨리는지 정하는 설정입니다.
+     * @property minRatio 돈을 떨어뜨리는 최소 비율
+     * @property maxRatio 돈을 떨어뜨리는 최대 비율
+     * @property maxAmount 떨어뜨리는 돈의 최댓값
      */
     private class MoneyConfig(var minRatio:Double = 0.05, var maxRatio:Double = 0.20, var maxAmount:Int = 300_0000){
         companion object{

4a0fa0e11b11a6caf0eabc23b3909dbe73f94cf8
RomeoRKPK <mindaye8504@gmail.com>
2022-12-31 20:38:08 +0900

    Fix: 일꾼 티켓 사용시 자격을 확인하는 제어문 추가

--- src/main/kotlin/com/danvhae/minecraft/siege/item/items/tickets/WorkTicket.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/items/tickets/WorkTicket.kt
@@ -2,6 +2,7 @@ package com.danvhae.minecraft.siege.item.items.tickets
 
 import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.objects.SiegeCastle
+import com.danvhae.minecraft.siege.core.objects.SiegePlayer
 import com.danvhae.minecraft.siege.item.abstracts.StarTicketAbstract
 import org.bukkit.entity.Player
 
@@ -9,6 +10,16 @@ class WorkTicket(destination:SiegeCastle) : StarTicketAbstract(destination) {
     override fun useAndTeleport(player: Player) {
         if(destination.status !in listOf(SiegeCastleStatus.PEACEFUL, SiegeCastleStatus.UNDER_BATTLE))
             return
+        //if(destination.team != SiegePlayer[player.uniqueId]?.team)
+        SiegePlayer[player.uniqueId].let {
+            if(it == null || (it.team != destination.team)){
+                player.sendMessage("일꾼 티켓을 사용할 수 없습니다. 정말 일하러 가는 것이 맞습니까?")
+                if(it == null){
+                    player.sendMessage("공성 플레이어 등록 절차를 먼저 진행하여주십시오")
+                }
+                return@useAndTeleport
+            }
+        }
         player.teleport(destination.workPosition)
         player.sendMessage("${destination.name} 일꾼 텔레포트 지점으로 이동하였습니다")
 

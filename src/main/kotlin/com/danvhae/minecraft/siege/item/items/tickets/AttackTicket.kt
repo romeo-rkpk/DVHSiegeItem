b1083e132bac28c171849a82396271ddbb4797ab
RomeoRKPK <mindaye8504@gmail.com>
2023-01-19 00:04:10 +0900

    Feat: 공성 티켓 제약조건 추가

--- src/main/kotlin/com/danvhae/minecraft/siege/item/items/tickets/AttackTicket.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/items/tickets/AttackTicket.kt
@@ -6,6 +6,7 @@ import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.core.objects.SiegeOperator
 import com.danvhae.minecraft.siege.core.objects.SiegePlayer
 import com.danvhae.minecraft.siege.core.utils.Hangul
+import com.danvhae.minecraft.siege.core.utils.LocationUtil
 import com.danvhae.minecraft.siege.item.abstracts.StarTicketAbstract
 import com.danvhae.minecraft.siege.item.utils.ItemUtil
 import org.bukkit.entity.Player
@@ -24,6 +25,9 @@ class AttackTicket(destination:SiegeCastle) : StarTicketAbstract(destination) {
             return
         }else if(!DVHSiegeCore.masterConfig.period){
             player.sendMessage("지금은 공성을 개시할 수 없습니다")
+            return
+        }else if(LocationUtil.attacking(player)){
+            player.sendMessage("공성 중에는 이 티켓을 사용할 수 없습니다")
             return
         }
         ItemUtil.targetItem(player, toItemStack())?.let { it.amount-- }

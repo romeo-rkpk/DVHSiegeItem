d5ac5fb8e5694619d93f0958a02993adcca5a5b6
RomeoRKPK <mindaye8504@gmail.com>
2023-01-19 14:21:26 +0900

    Feat: 티켓 사용 웅크리기 기능 요구사항 반영

--- src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/TicketItemListener.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/TicketItemListener.kt
@@ -15,11 +15,18 @@ class TicketItemListener: Listener {
         val player = event.player
 
         val ticket = TicketAbstract.parseTicket(event.item?:return)?:return
+        /*
         if(ticket is WildTicket){
             if(!player.isSneaking){
                 player.sendMessage("야생 티켓은 웅크린 상태에서만 사용할 수 있습니다")
                 return
             }
+        }
+
+         */
+        if(!player.isSneaking){
+            player.sendMessage("웅크린 상태에서 티켓을 사용할 수 있습니다")
+            return
         }
 
         ticket.useAndTeleport(player)

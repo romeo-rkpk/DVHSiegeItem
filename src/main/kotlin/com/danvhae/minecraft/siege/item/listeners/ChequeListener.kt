b2ee05a07bdc6862edd806be1af4b8018406343e
RomeoRKPK <mindaye8504@gmail.com>
2023-01-27 08:42:58 +0900

    Feat: 수표를 들 때 금액이 출력됨

--- src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/ChequeListener.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/item/listeners/ChequeListener.kt
@@ -3,6 +3,7 @@ package com.danvhae.minecraft.siege.item.listeners
 import com.danvhae.minecraft.siege.core.DVHSiegeCore
 import com.danvhae.minecraft.siege.core.utils.EconomyUtil
 import com.danvhae.minecraft.siege.core.utils.NumberUtil
+import com.danvhae.minecraft.siege.core.utils.TextUtil
 import com.danvhae.minecraft.siege.item.DVHSiegeItem
 import com.danvhae.minecraft.siege.item.events.UseChequeEvent
 import com.danvhae.minecraft.siege.item.items.Cheque
@@ -11,6 +12,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerItemHeldEvent
 
 class ChequeListener : Listener {
 
@@ -33,5 +35,12 @@ class ChequeListener : Listener {
         EconomyUtil.deposit(event.player, cheque.amount, "수표 사용", DVHSiegeItem.instance!!)
         event.player.sendMessage("수표를 사용하여 ${amount}(${NumberUtil.numberToHangul(amount)})스타" +
                 "를 얻었습니다.")
+    }
+
+    @EventHandler
+    fun onChequeHeld(event:PlayerItemHeldEvent){
+        Cheque.parseItem(event.player.inventory.getItem(event.newSlot)?:return)?.let {
+            TextUtil.sendActionBar(event.player, "&b&l${NumberUtil.numberToHangul(it.amount)} &a스타")
+        }
     }
 }
